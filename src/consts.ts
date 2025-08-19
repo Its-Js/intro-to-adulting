9bebdc33825a002e665ab0b52c7ed7ac67f396e0
Js <89004466+Its-Js@users.noreply.github.com>
2025-08-19 12:46:40 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'RL1001 - Introduction to Adulting';
+export const SITE_DESCRIPTION = 'A site intended to cover topics and areas that people entering the workforce need but never got taught on.';
